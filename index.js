a4cdc854d62edb9521e300b6d49ebbc51da8d145
Fauzi <fauzi190198@gmail.com>
2022-06-04 00:07:05 +0700

    feat: assign task callback

--- index.js
+++ index.js
@@ -6,15 +6,25 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, callback) => {
+  const result = callback(names);
+  const response = result.map((item, index) => `${index + 1}. ${item}`);
+  return response;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+  names.sort();
+  return names;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (names) => {
+  names.sort().reverse();
+  return names;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
